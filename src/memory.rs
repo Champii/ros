ac4f22a9f85ecb28427a8d357ed6e09b3bddcbd1
Champii <contact@champii.io>
2020-01-10 18:24:33 +0100

    checkpoint

--- src/memory.rs
+++ src/memory.rs
@@ -24,44 +24,121 @@ pub unsafe fn init() -> RecursivePageTable<'static> {
 use heapless::consts::*;
 use heapless::{ArrayLength, Vec};
 
-unsafe fn get_memory_map(multiboot_information_address: usize) -> &'static BootInformation {
-    unsafe { &multiboot2::load(multiboot_information_address) }
+pub unsafe fn load(address: usize) -> &'static BootInformation {
+    &*(address as *const BootInformation)
 }
 
-pub struct AvailableFrames(Vec<UnusedPhysFrame<Size4KiB>, U512>);
+unsafe fn get_memory_map(multiboot_information_address: usize) -> &'static BootInformation {
+    load(multiboot_information_address)
+}
 
-impl AvailableFrames {
-    pub fn new(multiboot_information_address: usize) -> Self {
-        let boot_info: BootInformation = unsafe { multiboot2::load(multiboot_information_address) };
+// // pub struct AvailableFrames(Vec<UnusedPhysFrame<Size4KiB>, U512>);
+// pub struct AvailableFrames(&'static BootInformation);
 
-        let elf_sections_tag = boot_info
-            .elf_sections_tag()
-            .expect("Elf-sections tag required");
+// impl AvailableFrames {
+//     pub fn new(multiboot_information_address: usize) -> Self {
+//         let boot_info: BootInformation = unsafe { multiboot2::load(multiboot_information_address) };
 
-        let kernel_start = elf_sections_tag
-            .sections()
-            .map(|s| s.start_address())
-            .min()
-            .unwrap();
-        let kernel_end = elf_sections_tag
-            .sections()
-            .map(|s| s.start_address() + s.size())
-            .max()
-            .unwrap();
+//         let elf_sections_tag = boot_info
+//             .elf_sections_tag()
+//             .expect("Elf-sections tag required");
 
-        let multiboot_start = multiboot_information_address as u64;
-        let multiboot_end = multiboot_start + (boot_info.total_size() as usize) as u64;
+//         let kernel_start = elf_sections_tag
+//             .sections()
+//             .map(|s| s.start_address())
+//             .min()
+//             .unwrap();
+//         let kernel_end = elf_sections_tag
+//             .sections()
+//             .map(|s| s.start_address() + s.size())
+//             .max()
+//             .unwrap();
 
-        let kernal_range = kernel_start..kernel_end;
-        let multiboot_range = multiboot_start..multiboot_end;
+//         let multiboot_start = multiboot_information_address as u64;
+//         let multiboot_end = multiboot_start + (boot_info.total_size() as usize) as u64;
+
+//         let kernal_range = kernel_start..kernel_end;
+//         let multiboot_range = multiboot_start..multiboot_end;
+
+//         let regions = boot_info.memory_map_tag().unwrap().memory_areas();
+
+//         let addr_ranges = regions.skip(1).map(|r| r.start_address()..r.end_address());
+
+//         let frame_addresses = addr_ranges
+//             .flat_map(|r| r.step_by(4096))
+//             .filter(|r| !kernal_range.contains(&r) && !multiboot_range.contains(&r));
+
+//         // for lol in frame_addresses.clone() {
+//         //     serial_println!("AVAILABLE FRAME : {:x}", lol);
+//         // }
+
+//         let frames = frame_addresses.map(|addr| PhysFrame::containing_address(PhysAddr::new(addr)));
+
+//         let res = frames.map(|f| unsafe { UnusedPhysFrame::new(f) });
+
+//         let mut collec = Vec::<UnusedPhysFrame<Size4KiB>, U512>::new();
+
+//         serial_println!("SIZE {}", res.clone().count());
+//         for frame in res {
+//             collec.push(frame);
+//         }
+
+//         Self(collec)
+//     }
+// }
+
+pub struct BootInfoFrameAllocator {
+    // available_frames: AvailableFrames,
+    multiboot_information_address: usize,
+    next: usize,
+}
+
+impl BootInfoFrameAllocator {
+    pub unsafe fn init(multiboot_information_address: usize) -> Self {
+        Self {
+            // boot_info: AvailableFrames::new(multiboot_information_address),
+            multiboot_information_address,
+            next: 0,
+        }
+    }
+
+    pub fn available_frames(&self) -> impl Iterator<Item = UnusedPhysFrame> {
+        // let boot_info: BootInformation = unsafe { multiboot2::load(multiboot_information_address) };
+
+        let boot_info: &'static BootInformation =
+            unsafe { load(self.multiboot_information_address) };
 
         let regions = boot_info.memory_map_tag().unwrap().memory_areas();
 
         let addr_ranges = regions.skip(1).map(|r| r.start_address()..r.end_address());
 
-        let frame_addresses = addr_ranges
-            .flat_map(|r| r.step_by(4096))
-            .filter(|r| !kernal_range.contains(&r) && !multiboot_range.contains(&r));
+        let frame_addresses = addr_ranges.flat_map(|r| r.step_by(4096)).filter(|r| {
+            let boot_info2: &'static BootInformation =
+                unsafe { load(self.multiboot_information_address) };
+
+            let elf_sections_tag = boot_info2
+                .elf_sections_tag()
+                .expect("Elf-sections tag required");
+
+            let kernel_start = elf_sections_tag
+                .sections()
+                .map(|s| s.start_address())
+                .min()
+                .unwrap();
+            let kernel_end = elf_sections_tag
+                .sections()
+                .map(|s| s.start_address() + s.size())
+                .max()
+                .unwrap();
+
+            let multiboot_start = boot_info2.start_address() as u64;
+            let multiboot_end = multiboot_start + (boot_info2.total_size() as u64);
+
+            let kernal_range = kernel_start..kernel_end;
+            let multiboot_range = multiboot_start..multiboot_end;
+
+            !kernal_range.contains(&r) && !multiboot_range.contains(&r)
+        });
 
         // for lol in frame_addresses.clone() {
         //     serial_println!("AVAILABLE FRAME : {:x}", lol);
@@ -71,35 +148,21 @@ impl AvailableFrames {
 
         let res = frames.map(|f| unsafe { UnusedPhysFrame::new(f) });
 
-        let mut collec = Vec::<UnusedPhysFrame<Size4KiB>, U512>::new();
+        // let mut collec = Vec::<UnusedPhysFrame<Size4KiB>, U512>::new();
 
-        serial_println!("SIZE {}", res.clone().count());
-        for frame in res {
-            collec.push(frame);
-        }
+        // serial_println!("SIZE {}", res.clone().count());
+        // for frame in res {
+        //     collec.push(frame);
+        // }
 
-        Self(collec)
-    }
-}
-
-pub struct BootInfoFrameAllocator {
-    available_frames: AvailableFrames,
-    next: usize,
-}
-
-impl BootInfoFrameAllocator {
-    pub unsafe fn init(multiboot_information_address: usize) -> Self {
-        Self {
-            available_frames: AvailableFrames::new(multiboot_information_address),
-            next: 0,
-        }
+        res
     }
 }
 
 unsafe impl FrameAllocator<Size4KiB> for BootInfoFrameAllocator {
     fn allocate_frame(&mut self) -> Option<UnusedPhysFrame> {
         // let frame = self.usable_frames().nth(self.next);
-        let frame = self.available_frames.0.iter().nth(self.next);
+        let frame = self.available_frames().nth(self.next);
         self.next += 1;
         frame.map(|f| unsafe { UnusedPhysFrame::new((*f).clone()) })
     }
